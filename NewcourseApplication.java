b2ba4299e1cd2fe9bfa726d570f12c2f44ad5f22
KunalBhujal <kunalb@heaptrace.net>
2023-03-13 18:18:44 +0530

    MVC pattern commit

---
+++ NewcourseApplication.java
@@ -0,0 +1,13 @@
+package com.javaht.springbootstarter.newcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewcourseApplication.class, args);
+	}
+
+}
